d2bae1ac812922a77c1e3389dc98b968a680945a
Kitson Kelly <me@kitsonkelly.com>
2021-08-03 14:21:13 +1000

    add logging

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
-import { Application, Router } from "https://deno.land/x/oak@v8.0.0/mod.ts";
+import {
+  Application,
+  HttpError,
+  Router,
+} from "https://deno.land/x/oak@v8.0.0/mod.ts";
+import * as colors from "https://deno.land/std@0.102.0/fmt/colors.ts";
 import { createGraph } from "https://raw.githubusercontent.com/kitsonk/deno_graph/main/mod.ts";
 
 const router = new Router();
@@ -28,5 +33,30 @@ const app = new Application();
 
 app.use(router.routes());
 app.use(router.allowedMethods());
+
+app.addEventListener("error", (evt) => {
+  let msg = `[${colors.red("error")}] `;
+  if (evt.error instanceof Error) {
+    msg += `${evt.error.name}: ${evt.error.message}`;
+  } else {
+    msg += Deno.inspect(evt.error);
+  }
+  if (
+    (evt.error instanceof HttpError && evt.error.status >= 400 &&
+      evt.error.status <= 499)
+  ) {
+    if (evt.context) {
+      msg += `\n\nrequest:\n  url: ${evt.context.request.url}\n  headers: ${
+        Deno.inspect([...evt.context.request.headers])
+      }\n`;
+    }
+  }
+  if (evt.error instanceof Error && evt.error.stack) {
+    const stack = evt.error.stack.split("\n");
+    stack.shift();
+    msg += `\n\n${stack.join("\n")}\n`;
+  }
+  console.error(msg);
+});
 
 addEventListener("fetch", app.fetchEventHandler());
