58912708146f78ad6e5d40696acc53b6fca5a653
Kitson Kelly <me@kitsonkelly.com>
2021-08-04 13:27:12 +1000

    update

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ import {
   Router,
 } from "https://deno.land/x/oak@v8.0.0/mod.ts";
 import * as colors from "https://deno.land/std@0.102.0/fmt/colors.ts";
-import { createGraph } from "https://raw.githubusercontent.com/kitsonk/deno_graph/main/mod.ts";
+import { createGraph } from "https://raw.githubusercontent.com/kitsonk/deno_graph/f2a50ae54666cd4a1e285dba02935f90cb73c490/mod.ts";
 
 const router = new Router();
 
